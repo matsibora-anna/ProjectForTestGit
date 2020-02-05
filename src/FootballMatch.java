4446fa6a7761b4f90d1b42874c413cca350eb068
Anna Matsibora <anna.matsibora@gmail.com>
2020-02-05 15:38:02 +0200

    initial commit

---
+++ src/FootballMatch.java
@@ -0,0 +1,9 @@
+public class FootballMatch {
+
+    public int matchResult(int result1, int result2, int result3, int result4) {
+
+        int result = 0;// can be 0, 1 , 2
+
+        return result;
+    }
+}
