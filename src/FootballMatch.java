3f54f9250819c0f2d9c2e8df9bda8b81e1d1230b
Anna Matsibora <anna.matsibora@gmail.com>
2020-02-05 18:03:18 +0200

    test commit add if

--- src/FootballMatch.java
+++ src/FootballMatch.java
@@ -3,7 +3,9 @@ public class FootballMatch {
     public int matchResult(int result1, int result2, int result3, int result4) {
 
         int result = 0;// can be 0, 1 , 2
-
+        if ( result1 == result3 ) {
+            result = 1;
+        }
         return result;
     }
 }
