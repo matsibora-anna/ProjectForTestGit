f18bc228f871c4d857b55931f6dc46d1b04ad659
Anna Matsibora <anna.matsibora@gmail.com>
2020-02-06 11:35:16 +0200

    add new java class StrongNumber

---
+++ src/StrongNumber.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class StrongNumber {
+    public static void main(String[] args) {
+        System.out.println(isStrongNumber(14));
+    }
+
+    private static String isStrongNumber(int num) {
+        ArrayList arrayNum = numberToArray(num);
+        int sumFactorialNumber = 0;
+
+        for(int i = 0; i < arrayNum.size(); i++) {
+            sumFactorialNumber += factorial((int) arrayNum.get(i));
+        }
+
+        if (sumFactorialNumber == num) return "STRONG!!!!";
+        else return "Not Strong !!";
+    }
+
+     // number > 0
+    private static ArrayList numberToArray (int number) {
+        ArrayList arrayList = new ArrayList();
+
+        while (number > 0) {
+            arrayList.add(number % 10);
+            number /= 10;
+        }
+
+        return arrayList;
+    }
+
+    // number >= 0
+    private static int factorial (int number) {
+        if(number == 0) return 1;
+        return factorial(number-1) * number;
+    }
+}
